ad47f5f72deeb7c6049bf30403f7ca3cd99cd587
José Rodrigo Lemos Pinto <rodlemoss@gmail.com>
2020-06-04 16:42:44 -0400

    Comentarios do que tem que ser feito

--- Farol/ViewController.swift
+++ Farol/ViewController.swift
@@ -6,6 +6,23 @@
 //  Copyright © 2020 yuryAntony. All rights reserved.
 //
 
+// TODO
+// Ainda é preciso:
+// CODIGO
+// 1 - Conectar a collectionView pra fazer o calendario
+// 1.1 - botar logica pra aparecer dia anterior quando apertar numa celula do calendario
+// 1.2 - implementar o calendario
+// 1.3 - fazer um calendario de 29 dias
+// 2 - fazer os passos que estao dentro do action saveButtonAction
+// 3 - fazer auto layout (DEIXAR PRO FINAL)
+
+// FORA CODIGO
+// 1 - Ver como saber a data do dia atual
+// 2 - onde verificar a data do dia e ver se ele ja esta no outro dia pra resetar a ui
+
+
+
+
 import UIKit
 
 class ViewController: UIViewController {
@@ -30,7 +47,14 @@ class ViewController: UIViewController {
             self.textViewInsight.textAlignment = .center
             self.textViewInsight.isHidden = false
         }
-        
+        // quando apertar o botão tem que:
+        // 1- iniciar animação da bola de loading e fazer a animação do farol
+        // depois que terminar as animações do farol e do loading, tem que:
+        // 1 - label "qual seu insight de hoje tem que mudar pro dia de hoje"
+        // 2 - mostrar insight no text view
+        // 3 - marcar a bola no calendario
+        // 4 - fazer o botão "salvar" sumir.
+        // 5 - mostrar a notificação que foi salva
     }
     
     
