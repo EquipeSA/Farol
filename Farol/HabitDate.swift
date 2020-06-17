ee93ed33e8bc26456743af880e2c8fadb497b18d
yury <yury.antonyb@gmail.com>
2020-06-17 13:41:18 -0400

    habitDate parcialmente adaptado

--- Farol/HabitDate.swift
+++ Farol/HabitDate.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class HabitDate: Codable {
     
-    init(day: String, weekDay: String, completed: Bool, selecionavel: Bool, habitDay: Bool, trashDays: Bool, insight: String?, date: String?, incompleted: Bool, badUI: Bool) {
+    init(day: String, weekDay: String, completed: Bool, selecionavel: Bool, habitDay: Bool, trashDays: Bool, insight: String?, date: String?, incompleted: Bool, badUI: Bool,scenes:SceneManager) {
         self.day = day
         self.weekDay = weekDay
         self.completed = completed
@@ -21,6 +21,7 @@ class HabitDate: Codable {
         self.date = date
         self.incompleted = incompleted
         self.badUI = badUI
+        self.scenes = scenes
     }
     
     let day: String
@@ -33,4 +34,7 @@ class HabitDate: Codable {
     var date: String?
     var incompleted: Bool
     var badUI: Bool
+    var scenes: SceneManager
 }
+
+import UIKit
